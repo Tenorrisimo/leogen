60c274452aa0ea2eeb96d88afff54083f40c33c5
vitalii-dmytruk <vdmytruk@intelliarts.com>
2016-10-22 18:50:45 +0300

    initial commit

---
+++ src/main/java/com/leogen/LoegenApplication.java
@@ -0,0 +1,12 @@
+package com.leogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoegenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/leogen/LoegenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.leogen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoegenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
